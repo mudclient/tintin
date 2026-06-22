a60b6fde78669490eb91c12baad8138f39d92d91
dzp <danzipao@gmail.com>
2026-06-23 00:43:12 +0800

    feat: support micro second in log

--- src/log.c
+++ src/log.c
@@ -259,15 +259,9 @@ void logit(struct session *ses, char *txt, FILE *file, int flags)
 
 	if (*ses->log->stamp_strf && (HAS_BIT(ses->log->mode, LOG_FLAG_STAMP) || file == ses->log->file))
 	{
-		if (ses->log->stamp_time != gtd->time)
-		{
-			ses->log->stamp_time = gtd->time;
-
-			substitute(ses, ses->log->stamp_strf, out, SUB_COL|SUB_ESC|SUB_VAR|SUB_FUN);
-
-			timestring(ses, out);
-			strcpy(ses->log->stamp_text, out);
-		}
+		substitute(ses, ses->log->stamp_strf, out, SUB_COL|SUB_ESC|SUB_VAR|SUB_FUN);
+		timestring(ses, out);
+		strcpy(ses->log->stamp_text, out);
 		fputs(ses->log->stamp_text, file);
 	}
 
